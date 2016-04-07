12773d63526ce9284d029111abdb5adbcfcac9e8
torreytsui <tt@amp.co>
2016-04-07 12:31:40 +0100

    Add show application state module

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'TorreyTsui_ShowApplicationState',
+    __DIR__
+);
